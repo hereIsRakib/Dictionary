f35e7ed7834312154b063fb22ee12ab9de9d43f4
hereIsRakib <hossain.rakib8011@gmail.com>
2023-02-04 23:26:38 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+dictionary = {
+    'a': 'mobile',
+    'b': 'bag',
+    'c': 'apple',
+    'd': 'books',
+    'e': 4
+}
+
+print(dictionary)
+print(dictionary['a'])
+dictionary['b'] = 'pen'
+print(dictionary)
